15a0ef5d5e0c0f3c7d2381fd2fc5c854fd6cc40e
ExMachina-N08 <aaron.le1995@gmail.com>
2024-10-09 21:14:23 +0700

    frontend production

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,9 @@ import react from "@vitejs/plugin-react-swc";
 export default defineConfig({
   plugins: [react()],
   //change port for production
-
+  build: {
+    outDir: "dist", // This is where the static files will be output
+  },
   // for dev
   server: {
     port: 3001,
