be2fd64c6512e750aa398fc643f0d67808d09a2a
ExMachina-N08 <aaron.le1995@gmail.com>
2024-10-09 21:22:01 +0700

    chunk size update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,19 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  //change port for production
   build: {
-    outDir: "dist", // This is where the static files will be output
+    outDir: "dist",
+    chunkSizeWarningLimit: 2000, // Increase if you have large bundles
+    rollupOptions: {
+      maxParallelFileOps: 2, // Reduce memory usage by limiting parallel file operations
+    },
   },
-  // for dev
   server: {
     port: 3001,
+  },
+  optimizeDeps: {
+    include: [], // Pre-bundle specific dependencies if needed
   },
 });
